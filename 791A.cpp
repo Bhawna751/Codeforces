eadadcc38080145e93edc7d62a04efe60e5f94ee
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-03-05 12:11:58 +0530

    Add files via upload

---
+++ 791A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int y=0;
+    while(a<=b){
+        a = a * 3;
+        b = b * 2;
+        y++; 
+    }
+    cout<<y;
+    return 0;
+}
